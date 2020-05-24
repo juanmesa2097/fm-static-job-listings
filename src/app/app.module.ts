35ef85284f42a762635c701ccc44f0393745f069
juanmesa2097 <juanmesa2097@gmail.com>
2020-05-24 04:35:42 -0500

    Create jobs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { JobsModule } from './features/jobs/jobs.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule],
+  imports: [BrowserModule, RouterModule, JobsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
