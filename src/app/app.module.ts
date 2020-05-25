2db8dc3e8a96f88d9885901373bf928f07241ba9
juanmesa2097 <juanmesa2097@gmail.com>
2020-05-25 01:36:24 -0500

    Create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './ui/header/header.module';
+import { LayoutModule } from './ui/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HeaderModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/ui/layout/layout.module.ts
+++ src/app/ui/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class HeaderModule {}
+export class LayoutModule {}
