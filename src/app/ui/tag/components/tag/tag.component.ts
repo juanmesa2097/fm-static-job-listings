0b3e267891204309215df79c8a55a76b9c063e74
juanmesa2097 <juanmesa2097@gmail.com>
2020-05-24 22:56:12 -0500

    Manage the state of every tag that matches

--- src/app/ui/tag/components/tag/tag.component.ts
+++ src/app/ui/tag/components/tag/tag.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TagsService } from '@app/core/services/tags.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.css'],
 })
-export class TagComponent implements OnInit {
+export class TagComponent implements OnInit, OnDestroy {
   @Input() tag: string;
   @Input() dismissible: boolean;
 
+  state = false;
+  subscription: Subscription;
+
   constructor(private tagsService: TagsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.tagsService.tagsSubject.subscribe(
+      (tags) => (this.state = tags.some((tag) => tag === this.tag))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   onChange(e) {
     const { value, checked } = e.target;
